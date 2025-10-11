0ec4650887c90a28e477b800c72821fb5be6cc4a
qq777linet <60238767+qq777linet@users.noreply.github.com>
2025-10-11 12:20:52 +0300

    1_15.c

---
+++ 1_15.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+
+int main()
+{
+    float F_to_C(float fahr)
+    {
+        float result;
+        result = (5.0/9.0)*(fahr - 32.0);
+        return result;
+    }
+    return 0;
+}
