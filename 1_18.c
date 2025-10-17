97efed913ed59c1c46c5b866222bdaa342af9c8b
qq777linet <60238767+qq777linet@users.noreply.github.com>
2025-10-17 21:48:02 +0300

    1_18.c

--- 1_18.c
+++ 1_18.c
@@ -1,26 +1,64 @@
 #include <stdio.h>
 
+#define MAXLINE 1000
+
 int main(void)
 {
-    char c;
-    int alarm = 0;
-    while((c = getchar()) != EOF)
+    char inline[MAXLINE];
+    char outline[MAXLINE];
+    int flag = 0;
+    int length;
+    
+    while((length = get_line(inline, MAXLINE)) != 0)
     {
-        if(c != ' ')
+        if(length == 1)
         {
-            printf("%c",c);
-            alarm = 0;
+            ;
         }
-        else if(c == ' ' && alarm == 0)
+        else
         {
-            printf("%c",c);
-            alarm = 1;
+            flag = 0;
+            for(int i = 0, j = 0; i < length; i++)
+            {
+                if((inline[i] == '\t' || inline[i] == ' ') && flag == 0)
+                {
+                    flag = 1;
+                    outline[j] = inline[i];
+                    j++;
+                }
+                else if((inline[i] == '\t' || inline[i] == ' ') && flag == 1)
+                {
+                    ;
+                }
+                else
+                {
+                    flag = 0;
+                    outline[j] = inline[i];
+                    j++;
+                }
+            }
+            outline[j] = '\0';
         }
-        else if(c == ' ' && alarm == 1)
-        {
-            printf("");
-            alarm = 1;
-        }
+            printf("%s", outline);
     }
-    printf('\n');
+
 }
+
+int get_line(char s[], int lim)
+{
+    int c, length;
+    for (length = 0; length < lim-1 && (c = getchar()) != EOF && c != '\n'; ++length)
+    {
+        s[length] = c;
+    }
+    if (c == '\n') 
+    {
+        s[length] = c;
+        ++length;
+    }
+    s[length] = '\0';
+    return length;
+}
+
+
+
