1013c6ef2d8a243374923963eda66d87a00a0881
qq777linet <60238767+qq777linet@users.noreply.github.com>
2025-10-26 18:59:13 +0200

    1_22.c

--- 1_22.c
+++ 1_22.c
@@ -1,28 +1,71 @@
 #include <stdio.h>
 
+#define N_POS 40
+
+int get_line(char s[], int lim)
+{
+    int c, i;
+    
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+        s[i] = c;
+    
+    s[i] = '\0';
+    
+    if (c == EOF)
+        return 1;
+    if (c == '\n')
+        return 2;
+    return 0;
+}
+
 int main(void)
 {
-    char c;
-    int counter = 0;
-    int max = 20;
-    while((c = getchar()) != EOF)
+    int test;
+    int counter;
+    char str[N_POS + 1];
+    char rest_buf[N_POS + 1] = "";  /
+    
+    while ((test = get_line(str, N_POS)) != 1)
     {
-        if((c != '\n') && (counter<max))
-        {
-            counter++;
-            printf("%c",c);
-        }
-        else if((c == '\n')&&(counter<max))
-        {
-            counter = 0;
-            printf("\n");
-        }
-        else if((c != '\n')&&(counter==max))
-        {
-            counter = 0;
-            printf("\n");
-        }і
 
+        if (rest_buf[0] != '\0')
+        {
+            int i = 0;
+            while (rest_buf[i] != '\0')
+            {
+                putchar(rest_buf[i]);
+                i++;
+            }
+            rest_buf[0] = '\0'; 
+        }
+
+        if (test == 2)
+        {
+            printf("%s\n", str);
+        }
+        else if (test == 0)
+        {
+
+            for (counter = N_POS - 1; counter > 0 && str[counter] != ' ' && str[counter] != '\t'; counter--)
+                ;
+
+            if (counter == 0)
+            {
+                printf("%s\n", str);
+            }
+            else
+            {
+                int j = 0;
+                for (int i = counter + 1; str[i] != '\0'; i++)
+                    rest_buf[j++] = str[i];
+                rest_buf[j] = '\0';
+                str[counter] = '\0';
+                printf("%s\n", str);
+            }
+        }
     }
-    printf("\n");
+    if (rest_buf[0] != '\0')
+        printf("%s\n", rest_buf);
+
+    return 0;
 }
