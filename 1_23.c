eb50a71dc30f4680bca354e05b71201b15d6ff88
qq777linet <60238767+qq777linet@users.noreply.github.com>
2025-10-26 21:25:08 +0200

    1_23.c

--- 1_23.c
+++ 1_23.c
@@ -1,26 +1,60 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main(void)
 {
-    char c;
-    int alarm = 0;
+    int flag = 0;
+    int flag_2 = 0;
+    int c;
+    
     while((c = getchar()) != EOF)
     {
-        if(c != '/')
+        if(c == '/' && flag == 0)
         {
-            alarm = 0;
-            printf("%c",c);
+            flag = 1;
         }
-        else if(c == '/' && alarm == 0)
+        else if(c == '/' && flag == 1)
         {
-            if((c = getchar()) == '/')
+            while((c = getchar()) != '\n')
             {
-               while((c == getchar()) != '\n')
-               {
-               }
+                ;
             }
+            printf("\n");
+            flag = 0;
+        }
+        else if(c != '/' && flag == 0)
+        {
+            printf("%c", c);
+        }
+        else if(c != '/' && c != '*' && flag == 1)
+        {
+            printf("/");
+            printf("%c", c);
+            flag = 0;
+        }
+        else if(c == '*' && flag == 1)
+        {
+            while((c = getchar()) != EOF) 
+            {
+                if(c == '*')
+                {
+                    flag_2 = 1;
+                }
+                
+                if(c != '*' && c != '/')
+                {
+                    flag_2 = 0;
+                }
+                
+                if(c == '/' && flag_2 == 1)
+                {
+                    printf("\n");
+                    break;
+                }
+                
+            }
+            flag = 0;
         }
 
     }
-    printf("\n");
+    return 0;
 }
