34fdddf3f67a8873899c55fa8ecba9dd53e8fb7e
qq777linet <60238767+qq777linet@users.noreply.github.com>
2025-11-11 22:57:53 +0200

    2_3.c

--- 2_3.c
+++ 2_3.c
@@ -1,42 +1,116 @@
-#include <stdio.h>
-#include <ctype.h>
-
-int htol(const char* s) 
+//Kernighan&Ritchie 2.3
+int htod(char a)
 {
-    int result = 0;  
-    int i = 0;
-
-
-    if (s[0] == '0' && (s[1] == 'x' || s[1] == 'X')) 
+    int b;
+    if(a == '1')
     {
-        i = 2;
+        b = 1;
+    }
+    else if(a == '2')
+    {
+        b = 2;
+    }
+    else if(a == '3')
+    {
+        b = 3;
+    }
+    else if(a == '4')
+    {
+        b = 4;
+    }
+    else if(a == '5')
+    {
+        b = 5;
+    }
+    else if(a == '6')
+    {
+        b = 6;
+    }
+    else if(a == '7')
+    {
+        b = 7;
+    }
+    else if(a == '8')
+    {
+        b = 8;
+    }
+    else if(a == '9')
+    {
+        b = 9;
+    }
+    else if(a == '0')
+    {
+        b = 0;
+    }
+    else if(a == 'A')
+    {
+        b = 10;
+    }
+    else if(a == 'B')
+    {
+        b = 11;
+    }
+    else if(a == 'C')
+    {
+        b = 12;
+    }
+    else if(a == 'D')
+    {
+        b = 13;
+    }
+    else if(a == 'E')
+    {
+        b = 14;
+    }
+    else if(a == 'F')
+    {
+        b = 15;
+    }
+    return b;
+}
+
+int power(int base, int exp) 
+{
+    int result = 1;
+
+    if (exp < 0) 
+    {
+        return 0;
     }
 
-
-    for (; s[i] != '\0'; i++) 
+    for (int i = 0; i < exp; i++) 
     {
-        char c = s[i];
-        int value;
-
-        if (isdigit(c)) 
-        {
-            value = c - '0';  
-        } 
-        else if (c >= 'a' && c <= 'f') 
-        {
-            value = c - 'a' + 10; 
-        } 
-        else if (c >= 'A' && c <= 'F') 
-        {
-            value = c - 'A' + 10;  
-        } 
-        else 
-        {
-            break;
-        }
-        result = result * 16 + value;
+        result *= base;
     }
 
     return result;
 }
 
+
+int htol(char hex[], int size)
+{
+    int result = 0;
+    if(size < 3)
+    {
+        return -1;
+    }
+    if(hex[0] == '0')
+    {
+        if(hex[1] == 'x')
+        {
+            for(int i = 0; i < size-2; i++)
+            {
+                result = result + htod(hex[i + 2]) * power(16, size - i - 3);
+            }
+            return result;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    else
+    {
+        return -1;
+    }
+}
