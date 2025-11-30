2ca59dc99b8e0d320d3d95d443e60b023bfef2f8
qq777linet <60238767+qq777linet@users.noreply.github.com>
2025-12-01 01:06:46 +0200

    3_1.c

--- 3_1.c
+++ 3_1.c
@@ -1,20 +1,25 @@
-int binsearch(int x, int array[], int n)
+//Kernighan&Ritchie 3.1
+
+int binsearch(int x, int v[], int n)
 {
     int low, high, mid;
     low = 0;
-    high = n - 1 ;
+    high = n - 1;
     mid = (low + high) / 2;
-    while (x != array[mid] && high>low) 
+
+    while(low<=high&&x!=v[mid])
     {
+        (x > v[mid])?(low = mid + 1):(high = mid - 1);
         mid = (low + high) / 2;
-        if (x > array[mid])
-        {
-            low = mid + 1;
-        }
-        else
-        {
-            high = mid - 1;
-        }
     }
-    return mid; 
-}
+    
+    if(v[mid] == x)
+    {
+        return mid;
+    }
+    else
+    {
+        return -1;
+    }
+        
+} 
