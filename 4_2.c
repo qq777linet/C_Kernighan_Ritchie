2f39308591b11d2f95914331e4ab20cc0a3aacef
qq777linet <60238767+qq777linet@users.noreply.github.com>
2025-12-25 22:35:22 +0200

    4_2.c

--- 4_2.c
+++ 4_2.c
@@ -1,37 +1,69 @@
+//Kernighan&Ritchie 4.2
 
 double atof (char s[])
 {
-  double val, power;
-  int i, sign;
-  int dec = 0;
-  for (i = 0; isspace (s[i]); i++)
-    ; 
-  sign = (s[i] == '-') ? -1 : 1;
-  if (s[i] == '+' || s[i] == '-')
-  {
-    i++;
-  }
-  for (val = 0.0; isdigit (s[i]); i++)
-  {
-    val = 10.0 * val + (s[i] - '0');
-  }
-  if (s[i] =='.')
-  {
-    i++;
-  }
-  for (power = 1.0; isdigit(s[i]); i++) 
-  {
-    val = 10.0 * val + (s[i] - '0');
-    power *= 10.0;
-  }
-  if (s[i] == 'e')
-  {
-    i++;
-    while(isdigit(s[i]))
+    double val, power;
+    int i, sign, e_sign;
+    int expon = 0;
+    for (i = 0; isspace (s[i]); i++)
     {
-      dec = dec*10 + (s[i] - '0');
-      i++;
+        ; 
     }
-  }
-  return sign * pow(10,dec) * val / power;
+    sign = (s[i] == '-') ? -1 : 1;
+    
+    if (s[i] == '+' || s[i] == '-')
+    {
+        i++;
+    }
+    
+    for (val = 0.0; isdigit (s[i]); i++)
+    {
+        val = 10.0 * val + (s[i] - '0');
+    }
+    
+    if (s[i] =='.')
+    {
+        i++;
+    }
+    
+    
+    for (power = 1.0; isdigit(s[i]); i++) 
+    {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
+    }
+    
+    if((s[i] == 'E')||(s[i] == 'e'))
+    {
+        i++;
+        if(s[i] == '-')
+        {
+            i++;
+            e_sign = 0;
+        }
+        else if(s[i] == '+')
+        {
+            i++;
+            e_sign = 1;
+        }
+        else
+        {
+            e_sign = 1;
+        }
+        while(isdigit(s[i]))
+        {
+            expon = 10.0 * expon + (s[i] - '0');
+            i++;
+        }
+        
+        if(e_sign == 1)
+        {
+            return sign * val / power * pow(10, expon);
+        }
+        else
+        {
+            return sign * val / power /pow(10,expon);
+        }
+    }
+    return sign * val / power;
 }
