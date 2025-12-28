a9b858ec43b11ce1b051b5dc2aaf72ecb0c45a44
qq777linet <60238767+qq777linet@users.noreply.github.com>
2025-12-28 23:23:07 +0200

    4_3.c

--- 4_3.c
+++ 4_3.c
@@ -2,9 +2,11 @@
 
 #include <stdio.h>
 #include <stdlib.h> // atof()
+#include <ctype.h>
 
 #define MAXOP 100 // max size of operand
 #define NUMBER '0' // digit indication
+#define MAXVAL 100 //max stack depth
 
 int getop (char []);
 void push (double);
@@ -53,4 +55,70 @@ int main(void)
             }
     }
  return 0;
+}
+
+int sp = 0; //next position in stack
+double val[ MAXVAL ]; //stack
+
+void push(double f) //puts value in stack
+{
+    if (sp < MAXVAL)
+    {    
+        val[sp++] = f;
+    }
+    else
+    {   
+        printf( "Error:stack overflow, %g does not fits\n", f);
+    }
+}
+
+double pop(void) //returns top of stack
+{
+    if (sp > 0)
+    {
+        return val[--sp];
+    }
+    else 
+    {
+        printf( "Error: stack is empty\п");
+        return 0.0;
+    }
+}
+
+int getch(void);
+void ungetch(int);
+
+int getop(char s[])//gets next operator or operand
+{
+    int i, c;
+    while ((s[0] = с = getch()) == ' ' || с == '\t' )
+    {
+        ;
+    }
+    s[1] = '\0';
+    if (!isdigit(c) && с != '.')
+    {
+        return с; //not digit
+    }
+    i = 0;
+    if (isdigit(c)) //collects integer part
+    {
+        while (isdigit(s[++i] = с = getch()))
+        {
+            ;
+        }
+    }
+    if (с == '.') //collects fractional part
+    {
+        while (isdigit(s[++i] = с = getch()))
+        {
+            ;
+        }
+    }
+    s[i] = '\0';
+    if (c != EOF)
+    {
+        ungetch(c);
+    }
+    return NUMBER;
 }
