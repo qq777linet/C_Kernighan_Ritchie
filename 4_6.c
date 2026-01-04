23c6e138f1e4085e744e3e991d8d7ad962ec2eea
qq777linet <60238767+qq777linet@users.noreply.github.com>
2026-01-04 23:11:19 +0200

    4_6.c

---
+++ 4_6.c
@@ -0,0 +1,285 @@
+//Kernighan&Ritchie 4.6
+
+#include <stdio.h>
+#include <stdlib.h> // atof()
+#include <ctype.h>
+#include <math.h>  // fmod
+#include <string.h>
+#include <stdbool.h>
+
+#define MAXOP 100 // max size of operand
+#define NUMBER '0' // digit indication
+#define MAXVAL 100 //max stack depth
+#define NAME 'n'
+#define VAR 'v'
+
+int getop (char []);
+void push (double);
+double pop (void);
+int getch(void);
+void ungetch(int);
+void print_stack_top(void);
+void duplicate(void);
+void change(void);
+void clean(void);
+
+// calculator with polish entry
+int main(void)
+{
+    int type;
+    double op2; //for temporary operand
+    char s[MAXOP]; //operand
+    while ((type = getop (s)) != EOF) 
+    {
+        switch (type) 
+        {
+            case NUMBER:
+                push (atof (s));
+                break;
+            case '+':
+                push (pop() + pop());
+                break;
+            case '*':
+                push (pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push (pop() - op2);
+                break;
+            case '?':
+                print_stack_top();
+                break;
+            case '/' :
+                op2 = pop();
+                if (op2 != 0.0)
+                {
+                    push (pop() / op2);
+                    break;
+                }
+                else
+                {
+                    printf("Error: division by zero\n");
+                    break;
+                }
+            case '%' :
+                op2 = pop();
+                if (op2 != 0.0)
+                {
+                    push (fmod(pop(), op2));
+                    break;
+                }
+                else
+                {
+                    printf("Error: division by zero\n");
+                    break;
+                }
+            case '=' :
+                break;
+                
+            case NAME:
+                if (strcmp(s, "sin") == 0) 
+                {
+                    push(sin(pop()));
+                }
+                else if (strcmp(s, "exp") == 0) 
+                {
+                    push(exp(pop()));
+                }
+                else if (strcmp(s, "duplicate") == 0) 
+                {
+                    duplicate();
+                }
+                else if (strcmp(s, "swap") == 0) 
+                {
+                    change();
+                }
+                else if (strcmp(s, "clean") == 0) 
+                {
+                    clean();
+                }
+                else if (strcmp(s, "pow") == 0) 
+                { 
+                    op2 = pop(); 
+                    push(pow(pop(), op2)); 
+                }
+                else
+                {
+                    printf("Error: unknown function %s\n", s);
+                }
+                break;
+            case VAR:
+                if(VAR_STATUS[atoi(s) - '0'] == TRUE)
+                {
+                    push(atof(s));
+                }
+                break;
+            case '\n' :
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("Error: unknown operation %s\n", s);
+                break;
+            }
+    }
+ return 0;
+}
+
+int sp = 0; //next position in stack
+double val[ MAXVAL ]; //stack
+double VAR_VAL[26]; // values of variables
+char VAR_ARAY[26]; // array of variable's names
+bool VAR_STATUS[26] = {false};
+for(int i = 0; i <26; i++)
+{
+    VAR_ARAY[i] = 97 + i;
+}
+
+void print_stack_top(void)
+{
+    if (sp > 0)
+    {
+        printf("\t%.8g\n", val[sp - 1]);
+    }
+    else
+    {
+            printf("Error: stack is empty\n");
+    }
+}
+
+void duplicate(void)
+{
+    if (sp > 0)
+    {
+        double temp = pop();
+        push(temp);
+        push(temp);
+    }
+    else
+    {
+        printf("Error: stack is empty\n");
+    }
+}
+
+void change(void)
+{
+    if (sp > 1)
+    {
+        double temp = pop();
+        double temp2 = pop();
+        push(temp);
+        push(temp2);
+    }
+    else
+    {
+        printf("Error: stack is too small\n");
+    }   
+}
+
+void clean(void)
+{
+    while(sp != 0)
+    {
+        pop();
+    }
+}
+
+void push(double f) //puts value in stack
+{
+    if (sp < MAXVAL)
+    {    
+        val[sp++] = f;
+    }
+    else
+    {   
+        printf( "Error:stack overflow, %g does not fits\n", f);
+    }
+}
+
+double pop(void) //returns top of stack
+{
+    if (sp > 0)
+    {
+        return val[--sp];
+    }
+    else 
+    {
+        printf( "Error: stack is empty\n");
+        return 0.0;
+    }
+}
+
+int getop(char s[])//gets next operator or operand
+{
+    int i, c; //i - counter; c - buffer
+    int next = 0; //for '-' handling in getop
+    while ((s[0] = c = getch()) == ' ' || c == '\t' ) //skip spaces and tabulation
+    {
+        ;
+    }
+    s[1] = '\0'; 
+    if (isalpha((unsigned char)c)) 
+    {
+        i = 0;
+        while (isalpha((unsigned char)c)) 
+        {
+            s[i++] = c;
+            c = getch();
+        }
+        s[i] = '\0';
+        if (c != EOF)
+            ungetch(c);
+    
+        return (i == 1) ? VAR : NAME;
+    }
+    if (!isdigit(c) && c != '.' && c!= '-')
+    {
+        return c; //not digit
+    }
+    i = 0;
+    if(c == '-')
+    {
+        next = getch();
+        if(!isdigit(next) && next != '.')
+        {
+            ungetch(next);
+            return c;
+        }
+        else
+        {
+            s[i] = c;
+            i++;
+            s[i] = next;
+            c = next;
+            i++;
+        }
+    }
+    if (isdigit(c)) //collects integer part
+    {
+        while (isdigit(s[++i] = c = getch()))
+        {
+            ;
+        }
+    }
+    if (c == '.') //collects fractional part
+    {
+        while (isdigit(s[++i] = c = getch()))
+        {
+            ;
+        }
+    }
+    s[i] = '\0';
+    if (c != EOF)
+    {
+        ungetch(c);
+    }
+    return NUMBER;
+}
+
+/* pseudocode
+
+- make array of names of variables
+- make array of doubles where values of variables are stored
+- changed getop (returns VAR)
+- case = 
+- case var 
+*/
