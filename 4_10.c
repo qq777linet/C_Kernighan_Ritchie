310e8f13623f68392689227c7c356b3571c67921
qq777linet <60238767+qq777linet@users.noreply.github.com>
2026-01-14 01:40:51 +0200

    4_10.c

--- 4_10.c
+++ 4_10.c
@@ -32,14 +32,69 @@ int pos = 0;
 
 int getop(char s[], char buffer[])//gets next operator or operand, returns type, stores in char s[]
 {
+    int i = 0;
     while(buffer[pos] == ' ')
     {
         pos ++; //skip spaces
     }
-    if(buffer[pos] == '\n')
+    if(buffer[pos] == '\n') 
     {
         return '\n';
     }
+    
+    if(isalpha(buffer[pos]))
+    {
+        if(buffer[pos+1] == ' ') //handling variables
+        {
+            s[0] = tolower(buffer[pos]);
+            s[1] = '\0';
+            return VAR;
+        }
+        else //handling strings
+        {
+            while(buffer[pos] != ' ')
+            {
+                s[i] = buffer[pos];
+                i++;
+                pos++;
+            }
+            s[i] = '\0';
+            return NAME;
+        }
+    }
+    if(buffer[pos] == '-') //handling - and -digits
+    {
+        if(buffer[pos+1] == ' ')
+        {
+            s[0] = '-';
+            s[1] = '\0';
+            pos+=2;
+            return '-';
+        }
+        else
+        {
+            while(buffer[pos] != ' ')
+            {
+                s[i] = buffer[pos];
+                i++;
+                pos++;
+            }
+            s[i] = '\0';
+            return NUMBER;
+        }
+    }
+    if(isdigit(buffer[pos]))
+    {
+        while(buffer[pos] != ' ')
+            {
+                s[i] = buffer[pos];
+                i++;
+                pos++;
+            }
+            s[i] = '\0';
+            return NUMBER;
+    }
+    return buffer[pos++];
 }
 /* pseudocode
 - return NUMBER
