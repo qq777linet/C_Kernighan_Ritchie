1bb61cc7c565ee5da88e90ddbae4f06933105751
qq777linet <60238767+qq777linet@users.noreply.github.com>
2026-05-19 23:38:42 +0300

    5_1.c

--- 5_1.c
+++ 5_1.c
@@ -1,38 +1,48 @@
+//Kernighan&Ritchie 5.1
+
 #include <ctype.h>
+#include <stdio.h>
+
 int getch (void);
+
 void ungetch (int);
-/* getint: читает следующее целое из ввода в *pn */
+
+/* getint: reads next int from input to *pn */
 int getint(int* pn)
 {
-    int c;
-    int sign;
+    int c, sign;
     while (isspace(c = getch()))
-        ;
-    if (!isdigit(c) && c != EOF && c ! = '+' && c != '-' ) 
     {
-        ungetch (c); 
+        ; /* skips spaces */
+    }
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-' ) 
+    {
+        ungetch (c); /* no digit */
         return 0;
     }
     sign = (c == '-' ) ? -1 : 1;
     if (c == '+' || c == '-')
     {
         c = getch();
-        if(isdigit(c))
-        {
-            ;
-        }
-        else
+        if(!isdigit(c))
         {
             ungetch(c);
-            ungetch(sign);
+            if(sign == 1)
+            {
+                ungetch('+');
+            }
+            else
+            {
+                ungetch('-');
+            }
             return 0;
         }
     }
     for (*pn = 0; isdigit(c); c = getch())
     {
         *pn = 10 * *pn + (c - '0' ) ;
-        *pn *= sign;
     }
+    *pn *= sign;
     if (c != EOF)
     {
         ungetch(c);
