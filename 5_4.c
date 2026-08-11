662e39de4ae8fdd3cb3aa0e0eadcdbae6cd03972
qq777linet <60238767+qq777linet@users.noreply.github.com>
2026-08-11 23:13:50 +0300

    5_4.c

--- 5_4.c
+++ 5_4.c
@@ -1,14 +1,34 @@
-void strend(char* s, char* t)
-{
-  int length_1 = strlen(s);
-  int length_2 = strlen(t);
 
-  for(int i = 0; i < length_2; i ++)
-  {
-    if(s[length_1 - length_2 + i] != t[i])
+// Kernighan&Ritchie 5.4 
+
+#include<stdio.h>
+
+//returns 1 if string t is located at the end of string s, and 0 if not 
+int strend(char* s, char* t)
+{
+    int T_LENGTH = 0;
+    while(*t != '\0')
     {
-      return 0; 
+        t++;
+        T_LENGTH++;
     }
-  }
-  return 1;
+    int S_LENGTH = 0;
+    while(*s != '\0')
+    {
+        s++;
+        S_LENGTH++;
+    }
+    s--;
+    t--;
+    while(T_LENGTH != 0)
+    {
+        if(*t != *s)
+        {
+            return 0;
+        }
+        t--;
+        s--;
+        T_LENGTH--;
+    }
+    return 1;
 }
